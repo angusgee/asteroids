b93792eec44681894c072aa3d0717342aaa9a53c
AngusG <maltawebdev@gmail.com>
2025-03-30 17:00:55 +0200

    Add player and circleshape classes

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame 
 from constants import * 
 
+import pygame
 
 def main(): 
     pygame.init()
+    pygame_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print('Starting Asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
@@ -15,6 +18,9 @@ def main():
                 return
         screen.fill('black')
         pygame.display.flip()
+        time_passed = pygame_clock.tick(60)
+        dt = time_passed / 1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
